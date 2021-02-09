260b39c59a9331b43352725d326777fdb2ca79c2
HY Tomioka <henriqueyudi2112@gmail.com>
2021-02-09 14:29:20 -0300

    Anotacao RestController e criacao TopicoDto

---
+++ src/main/java/br/com/tomioka/projetospring/controller/dto/TopicoDto.java
@@ -0,0 +1,46 @@
+package br.com.tomioka.projetospring.controller.dto;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import br.com.tomioka.projetospring.modelo.Topico;
+
+public class TopicoDto {
+
+	private Long id;
+	private String titulo;
+	private String mensagem;
+	private LocalDateTime dataCriacao;
+
+	public TopicoDto(Topico topico) {
+		this.id = topico.getId();
+		this.titulo = topico.getTitulo();
+		this.mensagem = topico.getMensagem();
+		this.dataCriacao = topico.getDataCriacao();
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public String getMensagem() {
+		return mensagem;
+	}
+
+	public LocalDateTime getDataCriacao() {
+		return dataCriacao;
+	}
+	
+	public static List<TopicoDto> converter(List<Topico> topicos) {
+		
+		System.out.println(topicos.stream().map(TopicoDto::new).collect(Collectors.toList()));
+		
+		return topicos.stream().map(TopicoDto::new).collect(Collectors.toList());
+	}
+
+}
