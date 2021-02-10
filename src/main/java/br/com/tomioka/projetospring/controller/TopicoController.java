9f23bc17a8cd35f1e3cc28dd9c5aa36a5c8cdff2
HY Tomioka <henriqueyudi2112@gmail.com>
2021-02-10 15:28:07 -0300

    Operacao CRUD: deletar Topico

--- src/main/java/br/com/tomioka/projetospring/controller/TopicoController.java
+++ src/main/java/br/com/tomioka/projetospring/controller/TopicoController.java
@@ -8,6 +8,7 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -47,6 +48,7 @@ public class TopicoController {
 	}
 
 	@PostMapping
+	@Transactional
 	public ResponseEntity<?> cadastrar(@RequestBody @Valid TopicoForm form, UriComponentsBuilder uriBuilder) {
 		Topico topico = form.converter(cursoRepository);
 		topicoRepository.save(topico);
@@ -74,6 +76,14 @@ public class TopicoController {
 		topico.setMensagem(form.getMensagem());
 		return ResponseEntity.ok().body(new TopicoDto(topico));
 	}
+	
+	@DeleteMapping("/{id}")
+	@Transactional
+	public ResponseEntity<?> deletar(@PathVariable Long id) {
+		topicoRepository.deleteById(id);
+		return ResponseEntity.ok().build();
+	}
+	
 	
 
 }
