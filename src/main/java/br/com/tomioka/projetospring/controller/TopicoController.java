ce7edc0fd7613ce83bb8a617512be5e9e09d26fe
HY Tomioka <henriqueyudi2112@gmail.com>
2021-02-09 23:38:07 -0300

    Implementacao metodo POST com form, em Topicos com status 201

--- src/main/java/br/com/tomioka/projetospring/controller/TopicoController.java
+++ src/main/java/br/com/tomioka/projetospring/controller/TopicoController.java
@@ -1,13 +1,16 @@
 package br.com.tomioka.projetospring.controller;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import br.com.tomioka.projetospring.controller.dto.TopicoDto;
 import br.com.tomioka.projetospring.controller.form.TopicoForm;
@@ -37,9 +40,13 @@ public class TopicoController {
 	}
 
 	@PostMapping
-	public void cadastrar(@RequestBody TopicoForm form) {
+	public ResponseEntity<TopicoDto> cadastrar(@RequestBody TopicoForm form, UriComponentsBuilder uriBuilder) {
 		Topico topico = form.converter(cursoRepository);
 		topicoRepository.save(topico);
+		
+		URI uri = uriBuilder.path("/topicos/{id}").buildAndExpand(topico.getId()).toUri();
+		// retorna o status 201 e o corpo da requisicao
+		return ResponseEntity.created(uri).body(new TopicoDto(topico));
 	}
 
 }
